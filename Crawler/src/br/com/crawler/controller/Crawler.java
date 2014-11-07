407ea9a246dc84d82ea65210217390090c2ee1a3
Petrus Carvalho <petrus.carvalho@dce.ufpb.br>
2014-11-07 12:07:44 -0300

    Arquivando comentarios outras paginas.

--- Crawler/src/br/com/crawler/controller/Crawler.java
+++ Crawler/src/br/com/crawler/controller/Crawler.java
@@ -17,66 +17,121 @@ import br.com.crawler.model.Conteudo;
  
 public class Crawler {
 	
+	private static final String URL = "http://www.tripadvisor.com.br/Hotel_Review-g190454-d3831132-Reviews-Palais_Hansen_Kempinski_Vienna-Vienna.html";
 	public static DatabaseDAO db = new DatabaseDAO();
 	public static int idSequencial = 1;
- 
+	private static int contadorPagComentario = 10;
+	private static int keyComentario = 1;
+	static Document doc = null;
+	
+	
 	public static void main(String[] args) throws SQLException, IOException {
 		
-		processPage("http://www.tripadvisor.com.br/Hotel_Review-g190454-d3831132-Reviews-Palais_Hansen_Kempinski_Vienna-Vienna.html", idSequencial);
+		
+		processPage();
 		
 	}
  
-	public static void processPage(String url, int key) throws SQLException, IOException{
+	public static void processPage() throws SQLException, IOException{
 	
-			Conteudo con = popularConteudo(url, key);	
+			Conteudo con = popularConteudo(URL);	
 			
-			Document doc = Jsoup.connect(url).get();
- 
-			Elements questions = doc.select("div.entry");
-			
-			for(Element link: questions){
-				
-				popularComentario(con, url, key, link.select("p").text());
-				System.out.println(link.select("p").text());
-				key = key + 1;
-				
+			if(doc == null){
+				doc =  Jsoup.connect(URL).get();
 			}
 			
+			processaComentarios(URL, con, doc);
+			//System.err.println("PASSOU!");
+			
+			processaArvoreComentarios(doc, URL);
+		
+	}
+
+	private static void processaArvoreComentarios(Document doc, String url)
+			throws SQLException, IOException {
+		
+		Elements questions = doc.select("a.guiArw");
+		
+		if((questions.size() == 1) && (questions.first().attr("abs:href").contains(montarOR()))){
+			processPage();
+		}
+		else{
+			for(Element link : questions){
+				if(link.attr("abs:href").contains(montarOR())){
+					processPage();
+				}
+				
+			}
+		}
+			
+			
+	}
+
+	private static String montarOR() {
+		
+		StringBuilder retorno = new StringBuilder("-or");
+		retorno.append(contadorPagComentario);
+		//System.out.println(retorno.toString());
+		contadorPagComentario = contadorPagComentario + 10;
+		
+		return retorno.toString();
+	}
+
+	private static void processaComentarios(String url, Conteudo con,
+			Document doc) throws SQLException, IOException {
+	
+		Elements questions = doc.select("div.entry");
+		
+		for(Element link: questions){
+			
+			popularComentario(con, url, link.select("p").text());
+			System.out.println(link.select("p").text());
+			keyComentario += 1;
+		}
 		
 	}
 
 
-	private static Conteudo popularConteudo(String url, int key) throws SQLException {
+	private static Conteudo popularConteudo(String url) throws SQLException {
 		
 		String sql = "";
 		Conteudo conteudo = new Conteudo();
 		sql = "INSERT INTO conteudo " + "(id,url) VALUES " + "(?,?);";
 		PreparedStatement stmt = db.conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 		
-		conteudo.setId(key);
+		conteudo.setId(idSequencial);
 		conteudo.setUrl(url);
 		stmt.setInt(1 , conteudo.getId());
 		stmt.setString(2, conteudo.getUrl());
 		stmt.execute();
 		
+		idSequencial += 1;
+		
 		return conteudo;
 		
 	}
 
-	private static void popularComentario(Conteudo c ,String url, int key, String comentario) throws SQLException, IOException {
+	private static void popularComentario(Conteudo c ,String url, String comentario) throws SQLException, IOException {
 		
 		String sql = "";
 		Comentario coment = new Comentario();
 		sql = "INSERT INTO comentario " + "(id,comentario,id_conteudo) VALUES " + "(?,?,?);";
 		PreparedStatement stmt = db.conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 		
-		coment.setId(key);
+		coment.setId(keyComentario);
 		coment.setConteudo(c);
 		stmt.setInt(1 , coment.getId());
 		stmt.setString(2, comentario);
 		stmt.setInt(3,  coment.getConteudo().getId());
 		stmt.execute();
-		
 			
+	}
+
+	public static int getContadorPagComentario() {
+		return contadorPagComentario;
+	}
+
+	public static void setContadorPagComentario(int contadorPagComentario) {
+		Crawler.contadorPagComentario = contadorPagComentario;
 	}	
 }
